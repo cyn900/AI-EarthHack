0650fd35d362a987de1af79fb6cbf0626413fe5d
cynthia <cyn.zhou@mail.utoronto.ca>
2024-01-07 13:40:02 -0500

    update

--- server/server.js
+++ server/server.js
@@ -23,84 +23,68 @@ app.post('/load-csv', upload.single('csvFile'), (req, res) => {
     const rows = [];
     let headers = null;
     let num = 0;
-
     try {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded' });
         }
-
         if (!req.body.evaluationGoal) {
             return res.status(400).json({ error: 'No evaluation goal provided' });
         }
-
         const evaluationGoal = req.body.evaluationGoal;  // do something with evaluation goal
         const fileBuffer = req.file.buffer;
         const fileContent = fileBuffer.toString('utf-8');
-
         const fs = require('fs');
         const path = require('path');
         
-        const outputCsvPath = path.join(__dirname, 'output.csv');
-        const writableStream = fs.createWriteStream(outputCsvPath, { flags: 'w' });
+         const outputCsvPath = path.join(__dirname, 'output.csv');
+         const writableStream = fs.createWriteStream(outputCsvPath, { flags: 'w' });
 
-        const newHeader = 'problem;solution;relevance;problemPopularityScore;problemPopularityExplaination;problemGrowingScore;problemGrowingExplaination;problemUrgentScore;problemUrgentExplaination;problemExpenseScore;problemExpenseExplaination;problemFrequentScore;problemFrequentExplaination;solutionCompletenessScore;solutionCompletenessExplaination;solutionCompletenessScore;solutionCompletenessExplaination;solutionTargetScore;solutionTargetExplaination;solutionNoveltyScore;solutionNoveltyExplaination;solutionFinImpactScore;solutionFinImpactExplaination;solutionImplementabilityScore;solutionImplementabilityExplaination;newName;tags;summary\n';
-        writableStream.write(newHeader);
+         const newHeader = 'problem;solution;relevance;problemPopularityScore;problemPopularityExplaination;problemGrowingScore;problemGrowingExplaination;problemUrgentScore;problemUrgentExplaination;problemExpenseScore;problemExpenseExplaination;problemFrequentScore;problemFrequentExplaination;solutionCompletenessScore;solutionCompletenessExplaination;solutionCompletenessScore;solutionCompletenessExplaination;solutionTargetScore;solutionTargetExplaination;solutionNoveltyScore;solutionNoveltyExplaination;solutionFinImpactScore;solutionFinImpactExplaination;solutionImplementabilityScore;solutionImplementabilityExplaination;newName;tags;summary\n';
+         writableStream.write(newHeader);
 
-        // Example function to write a row
+         // Example function to write a row
+    
         function writeRow(rowData) {
-            // const rowString = `${rowData.problem},${rowData.solution},${rowData.relevance},${rowData.problemPopularityScore},${rowData.problemPopularityExplaination},${rowData.problemGrowingScore},${rowData.problemGrowingExplaination},${rowData.problemUrgentScore},${rowData.problemUrgentExplaination},${rowData.problemExpenseScore},${rowData.problemExpenseExplaination},${rowData.problemFrequentScore},${rowData.problemFrequentExplaination},${rowData.solutionCompletenessScore},${rowData.solutionCompletenessExplaination},${rowData.solutionTargetScore},${rowData.solutionTargetExplaination},${rowData.solutionNoveltyScore},${rowData.solutionNoveltyExplaination},${rowData.solutionFinImpactScore},${rowData.solutionFinImpactExplaination},${rowData.solutionImplementabilityScore},${rowData.solutionImplementabilityExplaination},${rowData.newName},${rowData.tags},${rowData.summary}\n`;
-            const rowString = rowData;
+            const rowString = `${rowData.problem};${rowData.solution};${rowData.relevance};${rowData.problemPopularityScore};${rowData.problemPopularityExplaination};${rowData.problemGrowingScore};${rowData.problemGrowingExplaination};${rowData.problemUrgentScore};${rowData.problemUrgentExplaination};${rowData.problemExpenseScore};${rowData.problemExpenseExplaination};${rowData.problemFrequentScore};${rowData.problemFrequentExplaination};${rowData.solutionCompletenessScore};${rowData.solutionCompletenessExplaination};${rowData.solutionTargetScore};${rowData.solutionTargetExplaination};${rowData.solutionNoveltyScore};${rowData.solutionNoveltyExplaination};${rowData.solutionFinImpactScore};${rowData.solutionFinImpactExplaination};${rowData.solutionImplementabilityScore},${rowData.solutionImplementabilityExplaination};${rowData.newName};${rowData.tags};${rowData.summary}\n`;
             writableStream.write(rowString);
 }
-
         // Parse CSV content (using csv-parser as an example)
-
-        const tempFilePath = path.join(__dirname, 'tempFile.csv');
+        // const tempFilePath = path.join(__dirname, 'tempFile.csv');
         
-        // Write the uploaded content to the temporary file
-        fs.writeFileSync(tempFilePath, fileContent);
-
-        // Append an empty line to the temporary file
-        fs.appendFileSync(tempFilePath, '1,\n');
-
-        // Now read and process the temporary file
-        fs.createReadStream(tempFilePath)
-            .pipe(csv({ headers: false }))
-        // csv({ headers: false })
+        // // Write the uploaded content to the temporary file
+        // fs.writeFileSync(tempFilePath, fileContent);
+        // // Append an empty line to the temporary file
+        // fs.appendFileSync(tempFilePath, '\n');
+        // // Now read and process the temporary file
+        // fs.createReadStream(tempFilePath)
+        //     .pipe(csv({ headers: false }))
+        csv({ headers: false })
             .on('data', async(row) => {
                 console.log(row);
                 if (!headers) {
                     // First row is the header
                     headers = Object.values(row);
                 } else {
-                    console.log('1');
                     // Subsequent rows are the data
                     const rowData = {};
                     headers.forEach((header, index) => {
                         rowData[header] = row[index];
                     });
-
                     const prob = rowData['problem'];
                     // const solu = rowData['solution']
                     const prompt = 'Problem: ' + rowData['problem'] + 'Solution:' + rowData['solution'];
                     if (prob == null) {
                         return "None"; // or handle it as per your application's logic
                     }
-
                     const problemRegex = /Problem:\s*([^]+?)\.\s*Solution:/;
                     const problemMactch = prompt.match(problemRegex);
                     const problemDescription = problemMactch ? problemMactch[1].trim() : null;
-
                     //console.log(problemDescription);
-
                     num += 1;
                     console.log("num"+num);
                     //console.log(prompt);
-
                     const spamFilterReply = await spamFilter(prompt);
-
                     //console.log("reply" + spamFilterReply);
-
                     const problemPopularityReply = await problemPopularEval(prob);
                     const problemGrowingReply = await problemGrowingEval(prob);
                     const problemUrgentReply = await problemUrgentEval(prob);
@@ -114,15 +98,11 @@ app.post('/load-csv', upload.single('csvFile'), (req, res) => {
                     const generateNameReply = await generateName(prompt);
                     const generateTagsReply = await generateTags(prompt);
                     const generateSummaryReply = await generateSummary(prompt);
-
-
                     rowData.relevance = spamFilterReply; // "valid" or "invalid" where "valid means it is relevant, "invalid means it is a spam
                     //console.log(spamFilterReply);
-
                     rowData.problemPopularityScore = problemPopularityReply[0];
                     
                     //console.log("pop score: " + rowData['problemPopularityScore']);
-
                     rowData.problemPopularityExplaination = problemPopularityReply[1];
 
                     rowData.problemGrowingScore = problemGrowingReply[0];
@@ -154,23 +134,21 @@ app.post('/load-csv', upload.single('csvFile'), (req, res) => {
 
                     rowData.name = generateNameReply;
                     rowData.tags = generateTagsReply; // a list of tags ex: ['Water', 'Value'] 0<len(list)<=2
-                    rowData.summary = "\""+generateSummaryReply+"\"";
-
+                    rowData.summary = generateSummaryReply;
                     // rows.push(rowData);
-                    console.log('rowData: ' + rowData['problem']);
+                    // console.log('rowData: ' + rowData['problem']);
                     rows.push(rowData)
-                    writeRow(rowData['problem'] + "; "+rowData['solution']+"; " + rowData['relevance'] + "; " + rowData['problemPopularityScore'] + "; "+rowData['problemPopularityExplaination']+"; "+ rowData['problemGrowingScore']+ "; "+rowData['problemGrowingExplaination']+ "; "+rowData['problemUrgentScore']+ "; "+rowData['problemUrgentExplaination']+ "; "+rowData['problemExpenseScore']+ "; "+rowData['problemExpenseExplaination']+ "; "+ rowData['problemFrequentScore']+ "; "+rowData['problemFrequentExplaination']+ "; "+ rowData['solutionCompletenessScore']+ "; "+ rowData['solutionCompletenessExplaination']+ "; "+ rowData['solutionTargetScore']+ "; "+rowData['solutionTargetExplaination']+ "; "+ rowData['solutionNoveltyScore']+ "; "+rowData['solutionNoveltyExplaination']+ "; "+ rowData['solutionFinImpactScore']+ "; "+rowData['solutionFinImpactExplaination']+ "; "+ rowData['solutionImplementabilityScore']+ "; "+rowData['solutionImplementabilityExplaination']+ "; "+ rowData['newName']+ "; "+ rowData['tags']+ "; "+rowData['summary']);
+                    writeRow(rowData);
                     
                     // writeRow({problem: "\""+rowData['problem']+"\"", solution: "\""+rowData['solution']+"\"", relevance: rowData['relevance'], problemPopularityScore: rowData['problemPopularityScore'],problemPopularityExplaination: "\""+rowData['problemPopularityExplaination']+"\"", problemGrowingScore: rowData['problemGrowingScore'], problemGrowingExplaination: "\""+rowData['problemGrowingExplaination']+"\"", problemUrgentScore: rowData['problemUrgentScore'], problemUrgentExplaination: "\""+rowData['problemUrgentExplaination']+"\"", problemExpenseScore: rowData['problemExpenseScore'], problemExpenseExplaination: "\""+rowData['problemExpenseExplaination']+"\"", problemFrequentScore: rowData['problemFrequentScore'], problemFrequentExplaination: "\""+rowData['problemFrequentExplaination']+"\"", solutionCompletenessScore: rowData['solutionCompletenessScore'], solutionCompletenessExplaination: "\""+rowData['solutionCompletenessExplaination']+"\"", solutionTargetScore: rowData['solutionTargetScore'], solutionTargetExplaination: "\""+rowData['solutionTargetExplaination']+"\"", solutionNoveltyScore: rowData['solutionNoveltyScore'], solutionNoveltyScore: "\""+rowData['solutionNoveltyScore'], solutionNoveltyExplaination: "\""+rowData['solutionNoveltyExplaination']+"\"", solutionFinImpactScore: rowData['solutionFinImpactScore'], solutionFinImpactExplaination: "\""+rowData['solutionFinImpactExplaination']+"\"", solutionImplementabilityScore: rowData['solutionImplementabilityScore'], solutionImplementabilityExplaination: "\""+rowData['solutionImplementabilityExplaination']+"\"", newName: rowData['newName'], tags: rowData['tags'], summary: "\""+rowData['summary']+"\""});
                 }
             })
-            // .write(fileContent);
-        ''
+            .write(fileContent);
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-
     storedRows = rows;
     res.json({ csvData: rows });
     console.log(storedRows);
